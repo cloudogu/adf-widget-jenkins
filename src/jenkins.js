ff89706b2d169fa2e7e625ffe52d4c4c3fdb389c
Christoph Wolfes <christoph.wolfes@triology.de>
2016-05-09 09:36:16 +0200

    added endpoint constant for global configuration

--- src/jenkins.js
+++ src/jenkins.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var jenkinsWidget = angular.module('adf.widget.jenkins', ['adf.provider', 'chart.js'])
+  .constant("jenkinsEndpoint", {
+    "url": "http://builds.apache.org"
+  })
   .config(registerWidget);
 
 function registerWidget(dashboardProvider) {
@@ -10,11 +13,12 @@ function registerWidget(dashboardProvider) {
       description: 'Widget to display custom jenkins job',
       templateUrl: '{widgetsPath}/jenkins/src/view.html',
       resolve: {
-        data: function(jenkinsApi, config) {
+        data: function(jenkinsApi, config, jenkinsEndpoint) {
           if (config.apiUrl) {
-            return jenkinsApi.getJobData(config.apiUrl,config.project);
+            return jenkinsApi.getJobData(config.apiUrl, config.project);
+          } else if (jenkinsEndpoint.url && config.project) {
+            return jenkinsApi.getJobData(jenkinsEndpoint.url, config.project);
           }
-          return 'Please Setup the Widget';
         }
       },
       controller: 'jenkinsJobList',
@@ -28,9 +32,11 @@ function registerWidget(dashboardProvider) {
       description: 'Widget to diplay chart statistics from jenkins',
       templateUrl: '{widgetsPath}/jenkins/src/charts/view.html',
       resolve: {
-        data: function(jenkinsApi, config) {
+        data: function(jenkinsApi, config, jenkinsEndpoint) {
           if (config.apiUrl) {
             return jenkinsApi.getJobStats(config.apiUrl);
+          } else if (jenkinsEndpoint.url) {
+            return jenkinsApi.getJobStats(jenkinsEndpoint.url);
           }
           return 'Please Setup the Widget';
         }
