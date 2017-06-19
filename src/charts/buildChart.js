f3a070cc0ae02bbb9ba0a3c4765a0060f152a501
Christoph Wolfes <christoph.wolfes@triology.de>
2017-06-19 15:29:05 +0200

    switched color order

--- src/charts/buildChart.js
+++ src/charts/buildChart.js
@@ -8,7 +8,7 @@ jenkinsWidget.controller('jenkinsChart', function(data) {
   //configure chart
   var labels = ["Stable", "Fail","Unstable","Diabled"];
   var values = [jc.data.stable,jc.data.fail,jc.data.unstable,jc.data.disabled];
-  var colors = ['#B5CA00','#DD7800','#E43B53','#777777'];
+  var colors = ['#B5CA00','#E43B53','#DD7800','#777777'];
   jc.chartLabels = labels;
   jc.chartValues = values;
   jc.chartColors = colors;
