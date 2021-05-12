4d9c787deffcdac264c767f01040499e6e788619
jbartscher <jasper.bartscher@cloudogu.com>
2021-05-12 09:05:19 +0200

    Change key figure display element from `<h1>` to `<p>`. Upgraded gulp to v4.0.2. This is necessary because newer versions of node (currently v14.16.1) do not work with gulp v3.9. It also made it necessary to rewrite the gulpfile, since the execution of tasks changed a lot changed between v3 and v4. Add package-lock.json to VCS. Deletion of unused imports. Update of gulp-connect and gulp-inject library's.

--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,111 @@
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var wiredep = require('wiredep').stream;
-var $ = require('gulp-load-plugins')();
-var del = require('del');
-var jsReporter = require('jshint-stylish');
-var annotateAdfPlugin = require('ng-annotate-adf-plugin');
-var pkg = require('./package.json');
-const imagemin = require('gulp-imagemin');
+'use strict';
 
-var annotateOptions = {
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const wiredep = require('wiredep').stream;
+const $ = require('gulp-load-plugins')();
+const inject = require('gulp-inject');
+const del = require('del');
+const jsReporter = require('jshint-stylish');
+const annotateAdfPlugin = require('ng-annotate-adf-plugin');
+const pkg = require('./package.json');
+
+const annotateOptions = {
   plugin: [
     annotateAdfPlugin
   ]
 };
 
-var templateOptions = {
+const templateOptions = {
   root: '{widgetsPath}/jenkins/src',
   module: 'adf.widget.jenkins'
 };
 
 /** lint **/
 
-gulp.task('csslint', function(){
+gulp.task('csslint', function () {
   gulp.src('src/**/*.css')
-      .pipe($.csslint())
-      .pipe($.csslint.reporter());
+    .pipe($.csslint())
+    .pipe($.csslint.reporter());
 });
 
-gulp.task('jslint', function(){
+gulp.task('jslint', function () {
   gulp.src('src/**/*.js')
-      .pipe($.jshint())
-      .pipe($.jshint.reporter(jsReporter));
+    .pipe($.jshint())
+    .pipe($.jshint.reporter(jsReporter));
 });
 
-gulp.task('lint', ['csslint', 'jslint']);
+gulp.task('lint', gulp.series('csslint', 'jslint'));
 
 /** serve **/
 
-gulp.task('templates', function(){
+gulp.task('templates', () => {
   return gulp.src('src/**/*.html')
-             .pipe($.angularTemplatecache('templates.tpl.js', templateOptions))
-             .pipe(gulp.dest('.tmp/dist'));
+    .pipe($.angularTemplatecache('templates.tpl.js', templateOptions))
+    .pipe(gulp.dest('.tmp/dist'));
 });
 
-gulp.task('sample', ['templates'], function(){
-  var files = gulp.src(['src/**/*.js', 'src/**/*.css', 'src/**/*.less', '.tmp/dist/*.js'])
-                  .pipe($.if('*.js', $.angularFilesort()));
+gulp.task('sample', (done) => {
+  let files = gulp.src(['src/**/*.js', 'src/**/*.css', 'src/**/*.less', '.tmp/dist/*.js'])
+    .pipe($.if('*.js', $.angularFilesort()));
 
-  gulp.src(['sample/index.html'])
-      .pipe(wiredep({
-        directory: './components/',
-        bowerJson: require('./bower.json'),
-        devDependencies: true,
-        dependencies: true
-      }))
-      .pipe($.inject(files))
-      .pipe(gulp.dest('.tmp/dist'))
-      .pipe(connect.reload());
+  gulp.src('sample/index.html')
+    .pipe(wiredep({
+      directory: './components/',
+      bowerJson: require('./bower.json'),
+      devDependencies: true,
+      dependencies: true
+    }))
+    .pipe(inject(files))
+    .pipe(gulp.dest('.tmp/dist'));
+  // .pipe(connect.reload());
+  done();
 });
 
 
-gulp.task('watch', function(){
-  gulp.watch(['src/**'], ['sample']);
+gulp.task('watch', function (done) {
+  gulp.watch(['src/**'], gulp.series('sample'));
+  done();
 });
 
-gulp.task('serve', ['watch', 'sample'], function(){
+gulp.task('serve', gulp.series('templates', 'sample', 'watch', function () {
   connect.server({
     root: ['.tmp/dist', '.'],
     livereload: true,
     port: 9002
   });
-});
+}));
 
 /** build **/
 
-gulp.task('css', function(){
-  gulp.src(['src/**/*.css', 'src/**/*.less'])
-      .pipe($.if('*.less', $.less()))
-      .pipe($.concat(pkg.name + '.css'))
-      .pipe(gulp.dest('dist'))
-      .pipe($.rename(pkg.name + '.min.css'))
-      .pipe($.minifyCss())
-      .pipe(gulp.dest('dist'));
+gulp.task('css', function () {
+  return gulp.src(['src/**/*.css', 'src/**/*.less'])
+    .pipe($.if('*.less', $.less()))
+    .pipe($.concat(pkg.name + '.css'))
+    .pipe(gulp.dest('dist'))
+    .pipe($.rename(pkg.name + '.min.css'))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('js', function() {
-  gulp.src(['src/**/*.js', 'src/**/*.html'])
-      .pipe($.if('*.html', $.minifyHtml()))
-      .pipe($.if('*.html', $.angularTemplatecache(pkg.name + '.tpl.js', templateOptions)))
-      .pipe($.angularFilesort())
-      .pipe($.if('*.js', $.replace(/'use strict';/g, '')))
-      .pipe($.concat(pkg.name + '.js'))
-      .pipe($.headerfooter('(function(window, undefined) {\'use strict\';\n', '})(window);'))
-      .pipe($.ngAnnotate(annotateOptions))
-      .pipe(gulp.dest('dist'))
-      .pipe($.rename(pkg.name + '.min.js'))
-      .pipe($.uglify())
-      .pipe(gulp.dest('dist'));
+gulp.task('js', function () {
+  return gulp.src(['src/**/*.js', 'src/**/*.html'])
+    .pipe($.if('*.html', $.minifyHtml()))
+    .pipe($.if('*.html', $.angularTemplatecache(pkg.name + '.tpl.js', templateOptions)))
+    .pipe($.angularFilesort())
+    .pipe($.if('*.js', $.replace(/'use strict';/g, '')))
+    .pipe($.concat(pkg.name + '.js'))
+    .pipe($.headerfooter('(function(window, undefined) {\'use strict\';\n', '})(window);'))
+    .pipe($.ngAnnotate(annotateOptions))
+    .pipe(gulp.dest('dist'))
+    .pipe($.rename(pkg.name + '.min.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('dist'));
 });
 
 /** clean **/
-
-gulp.task('clean', function(cb){
+gulp.task('clean', function (cb) {
   del(['dist', '.tmp'], cb);
 });
 
-gulp.task('default',['css', 'js']);
+gulp.task('default', gulp.series('css', 'js'));
